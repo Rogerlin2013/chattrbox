9535ebcf02af5cd51236a506c31d146ab670018a
linyongzhi <yongzhi.lin@uama.com.cn>
2018-07-16 13:45:24 +0800

    增加对于读取文件出错的处理

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var http = require('http');
 var fs = require('fs');
 var extract = require('./extract');
 
+var handleError = function (err, res) {
+    res.writeHead(404);
+    res.end();
+}
+
 var server = http.createServer(function (req, res) {
     console.log('Responding to a request.');
 
@@ -9,7 +14,12 @@ var server = http.createServer(function (req, res) {
 
     var filePath = extract(url);
     fs.readFile(filePath, function (err, data) {
-        res.end(data);
+        if (err) {
+            handleError(err, res);
+            return;
+        }else {
+            res.end(data);
+        }
     });
 });
 server.listen(3000);
